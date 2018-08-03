7b8b6f87485af0ec1061a53ddcfdea4c4ecd3f11
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-08-03 17:00:49 +0530

    Initial commit

---
+++ CPP1.cpp
@@ -0,0 +1,29 @@
+// Program to enter 10 numbers and count total even and odd numbers and then perform the sum of even and odd numbers separately
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	clrscr();
+	int a[10],i,se=0,so=0,cte=0,cto=0;
+	cout<<"Enter 10 numbers\n";
+	for(i=0;i<10;i++)
+	{
+		cin>>a[i];
+		if(a[i]%2==0)
+		{
+			se=se+a[i];cte++;
+		}
+		else
+		{
+			so=so+a[i];
+			cto++;
+		}
+	}
+	cout<<"Total even numbers:"<<cte;
+	cout<<"\nTotal odd numbers:"<<cto;
+	cout<<"\nSum of even numbers:"<<se;
+	cout<<"\nSum of odd numbers:"<<so;
+	getch();
+}
+
