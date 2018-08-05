0c1d6cd9a44044f834bf71d624b6c38be631d1cb
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-08-05 22:22:09 +0530

    Program 2

---
+++ CPP2.cpp
@@ -0,0 +1,15 @@
+//Program to multiply two numbers without using '*'
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b,m=0,i;
+	cout<<"Enter 2 numbers: \n";
+	cin>>a>>b;
+	for(i=0;i<b;i++)
+		m=m+a;
+	cout<<"Output:"<<m;
+	getch();
+}
+
